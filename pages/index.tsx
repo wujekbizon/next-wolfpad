5cd0c5a88b538d8ffebd1fac831d0f312bf907d3
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-01-01 08:09:23 +0100

    first commit in 2023, adding a Link to wolfpad page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,29 @@
+import Link from 'next/link';
+
 const HomePage = () => {
   return (
     <main
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'space-evenly',
       }}
     >
-      <div>
+      <div style={{ color: 'white' }}>
         <h1>Home Page</h1>
-
-        <p>more coming soon...</p>
       </div>
+      <button
+        style={{
+          padding: '5px 15px',
+          fontSize: '18px',
+          backgroundColor: 'rgba(67, 30, 63, 1)',
+          color: 'white',
+        }}
+      >
+        <Link href="wolfpad">Wolfpad</Link>
+      </button>
     </main>
   );
 };
