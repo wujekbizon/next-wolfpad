97915a75d582c6a9539549260ef5bebca66d03d5
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-01-28 22:00:36 +0100

    adding AddCell component, adding Css

---
+++ components/AddCell/AddCell.tsx
@@ -0,0 +1,42 @@
+import styles from './AddCell.module.css';
+import { BiPlus } from 'react-icons/bi';
+import { useActions } from '../../hooks/useActions';
+
+interface AddCellProps {
+  prevCellId: string | null;
+  forceVisible?: boolean;
+}
+
+const AddCell: React.FC<AddCellProps> = ({ prevCellId, forceVisible }) => {
+  const { insertCellAfter } = useActions();
+
+  return (
+    <div
+      className={`${forceVisible && styles.force_visible} ${styles.add_cell}`}
+    >
+      <div className={styles.add_buttons}>
+        <button
+          className={styles.add_btn}
+          onClick={() => insertCellAfter({ id: prevCellId, type: 'code' })}
+        >
+          <span className={styles.icon}>
+            <BiPlus />
+          </span>
+          <span>Code</span>
+        </button>
+        <button
+          className={styles.add_btn}
+          onClick={() => insertCellAfter({ id: prevCellId, type: 'text' })}
+        >
+          <span className={styles.icon}>
+            <BiPlus />
+          </span>{' '}
+          <span>Text</span>
+        </button>
+      </div>
+      <div className={styles.divider} />
+    </div>
+  );
+};
+
+export default AddCell;
--- components/CellList/CellList.tsx
+++ components/CellList/CellList.tsx
@@ -2,6 +2,7 @@ import styles from './CellList.module.css';
 import React, { useEffect } from 'react';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
 import CellListItem from '../CellListItem/CellListItem';
+import AddCell from '../AddCell/AddCell';
 
 const CellList = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) =>
@@ -9,11 +10,13 @@ const CellList = () => {
   );
 
   return (
-    <div className="cell-list">
+    <div className={styles.cell_list}>
+      <AddCell prevCellId={null} forceVisible={cells.length === 0} />
       {cells.map((cell) => {
         return (
           <React.Fragment key={cell.id}>
             <CellListItem cell={cell} />
+            <AddCell prevCellId={cell.id} />
           </React.Fragment>
         );
       })}
