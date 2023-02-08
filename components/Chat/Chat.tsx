e8435b1f4086a78d3d783a9fadf2f9c9033aead4
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-02-08 20:17:55 +0100

    adding Chat component, css

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -1,7 +1,38 @@
 import styles from './Chat.module.css';
+import { motion } from 'framer-motion';
+import { staggerContainer, fadeIn } from '../../utils/motion';
 
 const Chat = () => {
-  return <h1>Chat</h1>;
+  return (
+    <main className={styles.chat}>
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={styles.content_container}
+      >
+        <motion.div
+          variants={fadeIn('right', 'tween', 0.4, 1)}
+          className={styles.content_wrapper}
+        >
+          <div className={styles.content}>
+            <h1 className={styles.title}>Chat</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+              suscipit totam aspernatur inventore neque ipsa aliquid deserunt
+              vero, quasi at aut pariatur eaque blanditiis minima molestiae
+              molestias optio iusto! Ut.
+            </p>
+          </div>
+        </motion.div>
+        <motion.div
+          variants={fadeIn('left', 'tween', 0.4, 1)}
+          className={styles.chat_container}
+        ></motion.div>
+      </motion.div>
+    </main>
+  );
 };
 
 export default Chat;
