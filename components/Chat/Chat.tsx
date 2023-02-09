32668509da1d2730241dd050f81d441ef92185e2
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-02-09 20:26:36 +0100

    adding logic into Chat component

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -1,17 +1,81 @@
 import styles from './Chat.module.css';
 import { motion } from 'framer-motion';
 import { staggerContainer, fadeIn } from '../../utils/motion';
-import { useRef } from 'react';
-
+import { useRef, useEffect, useMemo, useCallback, useState } from 'react';
+import fetchOpenAiApi from '../../helpers/chatApiCalls';
+import { generateUniqueId, chatStripe, loader } from '../../helpers/helpers';
 import InputForm from './InputForm';
+
+let loadInterval: NodeJS.Timer;
 
 const Chat = () => {
   const formRef = useRef<HTMLFormElement | null>(null);
-  const submitFormHandler: React.FormEventHandler<HTMLFormElement> = (
-    event
-  ) => {
-    event.preventDefault();
-  };
+  const chatContainerRef = useRef<HTMLElement | null>(null);
+  const [isInitializing, setIsInitializing] = useState(false);
+
+  const handleSubmitCallback: React.FormEventHandler<HTMLFormElement> =
+    useCallback(async (e) => {
+      e.preventDefault();
+
+      if (!formRef.current || !chatContainerRef.current) {
+        return;
+      }
+
+      const data = new FormData(formRef.current);
+
+      // user's chatstripe
+      chatContainerRef.current.innerHTML += chatStripe(
+        false,
+        data.get('prompt'),
+        ''
+      );
+
+      // to clear the textarea input
+      formRef.current.reset();
+
+      // bot's chatstripe
+      const uniqueId = generateUniqueId();
+      chatContainerRef.current.innerHTML += chatStripe(true, '', uniqueId);
+
+      // to focus scroll to the bottom
+      chatContainerRef.current.scrollTop =
+        chatContainerRef.current.scrollHeight;
+
+      // specific message div
+      const messageDiv = document.getElementById(uniqueId);
+
+      if (!messageDiv) {
+        return <p>Loading...</p>;
+      }
+
+      const interval = loader(messageDiv, loadInterval);
+      //  fetch api
+      const response = await fetchOpenAiApi(data, interval, messageDiv);
+
+      if (response.ok) {
+        setIsInitializing(true);
+      }
+    }, []);
+
+  const memoizeKeyPressHandler = useMemo(() => {
+    return (e: any) => {
+      if (e.keyCode === 13) {
+        handleSubmitCallback(e);
+      }
+    };
+  }, [handleSubmitCallback]);
+
+  useEffect(() => {
+    const form = formRef.current;
+    if (!form) {
+      return;
+    }
+    form.addEventListener('keydown', memoizeKeyPressHandler);
+
+    return () => {
+      form.removeEventListener('keydown', memoizeKeyPressHandler);
+    };
+  }, [memoizeKeyPressHandler]);
 
   return (
     <main className={styles.chat}>
@@ -40,7 +104,11 @@ const Chat = () => {
           variants={fadeIn('left', 'tween', 0.4, 1)}
           className={styles.chat_container}
         >
-          <InputForm formRef={formRef} onHandleSubmit={submitFormHandler} />
+          <section
+            className={styles.chat_inner}
+            ref={chatContainerRef}
+          ></section>
+          <InputForm formRef={formRef} onHandleSubmit={handleSubmitCallback} />
         </motion.div>
       </motion.div>
     </main>
