27e14dbb397045c432aa06dc99428a4eb723b44e
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-02-09 19:44:43 +0100

    adding helpers function for the chatbot

--- components/Chat/InputForm.tsx
+++ components/Chat/InputForm.tsx
@@ -1,6 +1,6 @@
 import styles from './InputForm.module.css';
 import Image from 'next/image';
-import Link from 'next/link';
+import Resizable from '../Resizable/Resizable';
 
 type InputFormProps = {
   onHandleSubmit: React.FormEventHandler<HTMLFormElement>;
@@ -9,17 +9,16 @@ type InputFormProps = {
 
 const InputForm = ({ onHandleSubmit, formRef }: InputFormProps) => {
   return (
-    <form className={styles.form} onSubmit={onHandleSubmit} ref={formRef}>
-      <textarea
-        name="prompt"
-        cols={1}
-        rows={1}
-        placeholder="Ask Jarvis..."
-      ></textarea>
-      <button className={styles.send}>
-        <Image src="/images/send.svg" alt="send" width={30} height={30} />
-      </button>
-    </form>
+    <>
+      {/* <Resizable direction="vertical"> */}
+      <form className={styles.form} onSubmit={onHandleSubmit} ref={formRef}>
+        <textarea name="prompt" placeholder="Ask Jarvis..."></textarea>
+        <button className={styles.send}>
+          <Image src="/images/send.svg" alt="send" width={40} height={70} />
+        </button>
+      </form>
+      {/* </Resizable> */}
+    </>
   );
 };
 export default InputForm;
