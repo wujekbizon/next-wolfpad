121466f5dd308117acc699e9c95d74aaf70f35f4
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-02-09 19:58:20 +0100

    creating fetchOpenAiApi function

---
+++ helpers/chatApiCalls.ts
@@ -0,0 +1,35 @@
+import { typeText } from './helpers';
+
+const fetchOpenAiApi = async (
+  data: FormData,
+  interval: NodeJS.Timer,
+  element: HTMLElement
+) => {
+  const response = await fetch('/api/chat', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt'),
+    }),
+  });
+
+  clearInterval(interval);
+  element.innerHTML = ' ';
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+
+    typeText(element, parsedData);
+  } else {
+    const err = await response.text();
+
+    element.innerHTML = 'Something went wrong';
+    console.log(err);
+  }
+  return response;
+};
+
+export default fetchOpenAiApi;
