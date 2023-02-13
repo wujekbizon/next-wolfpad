c578fea14e0e0c320ab0ffd7b302cdc0a102b663
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-02-13 18:36:44 +0100

    add responsive layout in GetStarted section

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -9,12 +9,16 @@ const Home = () => {
   return (
     <main className={styles.home_wrapper}>
       <Hero />
+
       <About />
       <div className="gradient_relative">
-        <div className="gradient-04" />
+        <div className="gradient-03" />
       </div>
       <GetStarted />
       <Explore />
+      <div className="gradient_relative">
+        <div className="gradient-04" />
+      </div>
       <WhatsNew />
       <Founder />
       <Footer />
