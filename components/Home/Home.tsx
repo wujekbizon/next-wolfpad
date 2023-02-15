8355b083b2dc063c1f3dff9fbda4a420e73ca62a
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-02-15 10:31:46 +0100

    sharing SidebarMenu with other pages in Layout component

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -4,15 +4,10 @@ import WhatsNew from '../../sections/WhatsNew';
 import Explore from '../../sections/Explore';
 import Footer from '../../sections/Footer';
 import Founder from '../../sections/Founder';
-import { SidebarMenu } from '../Animation/SidebarMenu';
-import { useTypedSelector } from '../../hooks/useTypedSelector';
 
 const Home = () => {
-  const isMenuOpen = useTypedSelector((state) => state.modals.isMenuOpen);
-
   return (
     <main className={styles.home_wrapper}>
-      {isMenuOpen && <SidebarMenu />}
       <Hero />
       <About />
       <div className="gradient_relative">
--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,13 +1,17 @@
 import Navbar from '../Navbar/Navbar';
+import { SidebarMenu } from '../Animation/SidebarMenu';
+import { useTypedSelector } from '../../hooks/useTypedSelector';
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Layout = ({ children }: Props) => {
+  const isMenuOpen = useTypedSelector((state) => state.modals.isMenuOpen);
   return (
     <>
       <Navbar />
+      {isMenuOpen && <SidebarMenu />}
       <main>{children}</main>
     </>
   );
