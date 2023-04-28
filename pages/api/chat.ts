0972e41365c7196faae2c3cb8635311df495516f
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-04-28 22:07:43 +0200

    minor change to chatBotAPI

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,10 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { Configuration, OpenAIApi, ChatCompletionRequestMessage, ChatCompletionRequestMessageRoleEnum } from 'openai'
-
-interface Chatbot extends ChatCompletionRequestMessage {
-  role: ChatCompletionRequestMessageRoleEnum
-  content: string
-}
+import { Configuration, OpenAIApi } from 'openai'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'GET') {
@@ -18,45 +13,27 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
     const openai = new OpenAIApi(configuration)
     const { prompt } = req.body
-    let history: any = []
 
-    while (true) {
-      let messages: Chatbot[] = []
+    try {
+      const response = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
 
-      for (const [input_text, completion_text] of history) {
-        messages.push({ role: 'user', content: input_text })
-        messages.push({ role: 'assistant', content: completion_text })
-      }
+        messages: [{ role: 'user', content: prompt }],
+        temperature: 0,
+        max_tokens: 200,
+        top_p: 1,
+        frequency_penalty: 0.5,
+        presence_penalty: 0
+      })
 
-      messages.push({ role: 'user', content: prompt })
-
-      try {
-        const response = await openai.createChatCompletion({
-          model: 'gpt-3.5-turbo',
-
-          messages: messages,
-          // temperature: 0,
-          max_tokens: 200
-          // top_p: 1,
-          // frequency_penalty: 0.5,
-          // presence_penalty: 0
-        })
-
-        console.log(history, messages)
-
-        const completion_text = response.data.choices[0]?.message?.content
-
-        history.push([prompt, completion_text])
-
-        return res.status(200).json({
-          bot: completion_text
-        })
-      } catch (error) {
-        if (error instanceof Error) {
-          return res.status(500).json({ message: error.message })
-        } else {
-          console.log(error)
-        }
+      res.status(200).json({
+        bot: response.data.choices[0]?.message?.content
+      })
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(500).json({ message: error.message })
+      } else {
+        console.log(error)
       }
     }
   }
