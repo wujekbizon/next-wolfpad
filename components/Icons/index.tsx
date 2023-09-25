25d232f9a844261e40c9d06d0319c818b617f491
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-09-25 10:28:23 +0200

    add motion icons

---
+++ components/Icons/index.tsx
@@ -0,0 +1,25 @@
+import Supply from './Supply'
+import ProgressiveWeb from './ProgressiveWeb'
+import EnterpriseWeb from './EnterpriseWeb'
+import WebPortals from './WebPortals'
+import CloudWeb from './CloudWeb'
+import WebConsulting from './WebConsulting'
+import Designer from './Designer'
+import Backend from './Backend'
+import Frontend from './Frontend'
+import Manager from './Manager'
+import Engineer from './Engineer'
+
+export {
+  Supply,
+  ProgressiveWeb,
+  EnterpriseWeb,
+  WebPortals,
+  CloudWeb,
+  WebConsulting,
+  Designer,
+  Backend,
+  Frontend,
+  Manager,
+  Engineer,
+}
---
+++ types/icons.d.ts
@@ -0,0 +1,6 @@
+interface IconAttributes {
+  width?: number
+  height?: number
+  primary?: string
+  secondary?: string
+}
